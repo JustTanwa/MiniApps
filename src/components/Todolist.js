fb618e74d0f45e91a63b93ef880b77cacda7b6ea
Katakung95 <tanwasripan@hotmail.co.uk>
2022-01-13 16:31:04 +0000

    Added spin effects on remove

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Textbox } from "./Textbox";
 
 function Todolist() {
+    const [task, setTask] = useState('')
+    const [taskList, setTaskList] = useState([{}]);
+
+    useEffect(() => {
+        let nextId = taskList.length > 1 ? taskList[taskList.length - 1].id + 1 : 1;
+        if (task !== "") {
+            setTaskList((prev) => [...prev, { task: task, id: nextId }]);
+            setTask("");
+        }
+    }, [task, taskList]);
+
+    const updateTask = (newTask) => {
+        setTask(newTask);
+    }
+    const handleDelete = (id) => {
+        const newList = taskList.filter((item) => item.id !== id);
+        if (newList.length > 0) {
+            setTaskList(newList);
+        } else {
+            setTaskList([]);
+        }
+    }
+
+    const handleSpin = (event, id) => {
+        event.target.classList.add("spinning");
+        const deleteTimer = setTimeout(() => handleDelete(id), 1200);
+        
+    }
+
     return (
         <div className="container-fluid bg-green vh-90" id="todolist">
             <section>
                 <h3 className="text-center">To do list</h3>
-                <Textbox className="mr-10 ml-10"/>
+                <Textbox updateOption={updateTask} />
+                <div className="container-fluid">
+                    <div className="row">
+                        {taskList.map((item) => {
+                            if (!(Object.keys(item).length === 0 && item.constructor === Object)) {
+                                return (
+                                    <div className="col-sm-3 p-3" key={item.id}>
+                                        <div className="myCard">
+                                            <button type="button" onClick={(e) => handleSpin(e, item.id)} className='card-inner'>{item.task}</button>
+                                        </div>
+                                    </div>
+                                )
+                            } else { return null }
+                        })}
+                    </div>
+                </div>
             </section>
         </div>
     )
